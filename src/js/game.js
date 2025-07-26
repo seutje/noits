633bddadfa346c3be46802ba0c30bca93bd16467
seutje <seutje@gmail.com>
2025-07-26 11:57:57 +0200

    feat: Use dirt sprite

--- src/js/game.js
+++ src/js/game.js
@@ -69,6 +69,7 @@ export default class Game {
             await this.spriteManager.loadImage('stone', 'src/assets/stone.png');
             await this.spriteManager.loadImage('iron_ore', 'src/assets/iron_ore.png');
             await this.spriteManager.loadImage('deer', 'src/assets/deer.png');
+            await this.spriteManager.loadImage('dirt', 'src/assets/dirt.png');
             await this.soundManager.loadSound('action', ACTION_BEEP_URL);
         } catch (error) {
             console.error("Failed to load sprite:", error);
--- src/js/map.js
+++ src/js/map.js
@@ -96,6 +96,7 @@ export default class Map {
         const stoneSprite = this.spriteManager.getSprite('stone');
         const ironOreSprite = this.spriteManager.getSprite('iron_ore');
         const deerSprite = this.spriteManager.getSprite('deer');
+        const dirtSprite = this.spriteManager.getSprite('dirt');
 
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
@@ -118,6 +119,8 @@ export default class Map {
                     ctx.drawImage(ironOreSprite, x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
                 } else if (tile === 7 && deerSprite) {
                     ctx.drawImage(deerSprite, x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
+                } else if (tile === 1 && dirtSprite) {
+                    ctx.drawImage(dirtSprite, x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);
                 } else if (tile !== 0) {
                     ctx.fillStyle = this.tileColors[tile] || '#000000';
                     ctx.fillRect(x * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize);

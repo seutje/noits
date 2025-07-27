e6aa1bb78fe716c2800404086995e1f7885795c8
Steve De Jonghe <seutje@gmail.com>
2025-07-27 03:10:20 +0200

    Prevent new gathering tasks while hauling

---
+++ __tests__/GameGatheringBehavior.test.js
@@ -0,0 +1,24 @@
+import Game from '../src/js/game.js';
+import Settler from '../src/js/settler.js';
+import Task from '../src/js/task.js';
+
+describe('Game gathering behavior', () => {
+    test('settler carrying resource ignores new gather tasks', () => {
+        const ctx = { canvas: { width: 800, height: 600 }, clearRect: jest.fn(), drawImage: jest.fn() };
+        const game = new Game(ctx);
+        const settler = new Settler('Bob', 0, 0, game.resourceManager, game.map, game.roomManager, game.spriteManager, game.settlers);
+        settler.updateNeeds = jest.fn();
+        settler.carrying = { type: 'wood', quantity: 1 };
+        settler.state = 'idle';
+        game.settlers.push(settler);
+        game.roomManager.setSettlers(game.settlers);
+
+        const gatherTask = new Task('chop_wood', 0, 0, 'wood', 1, 2);
+        game.taskManager.addTask(gatherTask);
+
+        game.update(16);
+
+        expect(settler.currentTask).toBe(null);
+        expect(game.taskManager.tasks.length).toBe(1);
+    });
+});

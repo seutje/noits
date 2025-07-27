3ee09d0d424d8d47d71c55aff94f52d87686df9c
Steve De Jonghe <seutje@gmail.com>
2025-07-27 15:53:07 +0200

    Remove empty resource piles automatically

--- __tests__/ResourcePile.test.js
+++ __tests__/ResourcePile.test.js
@@ -11,4 +11,14 @@ describe('ResourcePile', () => {
         const pile = new ResourcePile('stone', 1200, 0, 0, 32, {});
         expect(pile.quantity).toBe(999);
     });
+
+    test('remove should notify map when empty', () => {
+        const mockMap = { removeResourcePile: jest.fn() };
+        const pile = new ResourcePile('wood', 5, 0, 0, 32, {});
+        pile.map = mockMap;
+        const result = pile.remove(5);
+        expect(result).toBe(true);
+        expect(pile.quantity).toBe(0);
+        expect(mockMap.removeResourcePile).toHaveBeenCalledWith(pile);
+    });
 });

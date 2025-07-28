d58889ea9c9e7458c277e4182b9d0a4c6e6c6a35
Steve De Jonghe <seutje@gmail.com>
2025-07-28 20:43:51 +0200

    Move sprite list to constants (#115)

--- src/js/constants.js
+++ src/js/constants.js
@@ -121,3 +121,47 @@ export const GATHER_TASK_TYPES = new Set([
 // Movement speeds
 export const SETTLER_RUN_SPEED = 0.5; // tiles per second
 export const ENEMY_RUN_SPEED = 0.3; // tiles per second
+
+// Sprite definitions used during game startup
+export const SPRITES = [
+  ['settler', 'src/assets/settler.png'],
+  ['tree', 'src/assets/tree.png'],
+  ['grass', 'src/assets/grass.png'],
+  ['water', 'src/assets/water.png'],
+  ['berry_bush', 'src/assets/berry_bush.png'],
+  ['goblin', 'src/assets/goblin.png'],
+  [RESOURCE_TYPES.STONE, 'src/assets/stone.png'],
+  [RESOURCE_TYPES.IRON_ORE, 'src/assets/iron_ore.png'],
+  ['deer', 'src/assets/deer.png'],
+  [RESOURCE_TYPES.DIRT, 'src/assets/dirt.png'],
+  ['wild_boar', 'src/assets/wild_boar.png'],
+  ['mushroom', 'src/assets/mushroom.png'],
+  ['mushrooms', 'src/assets/mushrooms.png'],
+  [RESOURCE_TYPES.WOOD, 'src/assets/wood.png'],
+  [RESOURCE_TYPES.PLANK, 'src/assets/plank.png'],
+  [RESOURCE_TYPES.BUCKET, 'src/assets/bucket.png'],
+  ['stone_pile', 'src/assets/stone_pile.png'],
+  [RESOURCE_TYPES.BERRIES, 'src/assets/berries.png'],
+  [RESOURCE_TYPES.MEAT, 'src/assets/meat.png'],
+  ['dirt_pile', 'src/assets/dirt_pile.png'],
+  [BUILDING_TYPES.FARM_PLOT, 'src/assets/farmPlot.png'],
+  [RESOURCE_TYPES.BANDAGE, 'src/assets/bandage.png'],
+  [BUILDING_TYPES.CRAFTING_STATION, 'src/assets/crafting_station.png'],
+  [BUILDING_TYPES.OVEN, 'src/assets/oven.png'],
+  [RESOURCE_TYPES.BREAD, 'src/assets/bread.png'],
+  [RESOURCE_TYPES.MEAL, 'src/assets/meal.png'],
+  [BUILDING_TYPES.TABLE, 'src/assets/table.png'],
+  [BUILDING_TYPES.BED, 'src/assets/bed.png'],
+  [BUILDING_TYPES.WELL, 'src/assets/well.png'],
+  [RESOURCE_TYPES.BUCKET_WATER, 'src/assets/bucket_water.png'],
+  ['wheat_1', 'src/assets/wheat_1.png'],
+  ['wheat_2', 'src/assets/wheat_2.png'],
+  ['wheat_3', 'src/assets/wheat_3.png'],
+  ['wheat_pile', 'src/assets/wheat_pile.png'],
+  ['cotton_1', 'src/assets/cotton_1.png'],
+  ['cotton_2', 'src/assets/cotton_2.png'],
+  ['cotton_3', 'src/assets/cotton_3.png'],
+  ['cotton_pile', 'src/assets/cotton_pile.png'],
+  ['iron_ore_pile', 'src/assets/iron_ore_pile.png'],
+  ['construction', 'src/assets/construction.png']
+];
